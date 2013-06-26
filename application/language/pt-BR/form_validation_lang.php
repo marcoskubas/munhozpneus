0213f37d9180b79b2cf5cfcb3d7b9cc021304fb3
Daiane Ambos <duda.ambos@gmail.com>
2013-06-26 20:03:54 -0300

    Ajuste da codificação das mensagens do formulário

--- application/language/pt-BR/form_validation_lang.php
+++ application/language/pt-BR/form_validation_lang.php
@@ -1,21 +1,21 @@
 <?php
 
-$lang['required'] = 'O campo %s é obrigatório.';
+$lang['required'] = 'O campo %s &eacute; obrigat&oacute;rio.';
 $lang['isset'] = 'O campo %s deve ter algum valor.';
-$lang['valid_email'] = 'O campo %s deve conter um endereço de e-mail válido.';
-$lang['valid_emails'] = 'O campo %s deve conter todos os endereços de e-mails válidos.';
-$lang['valid_url'] = 'O campo %s deve conter uma URL válida.';
-$lang['valid_ip'] = 'O campo %s deve conter um IP válido.';
+$lang['valid_email'] = 'O campo %s deve conter um endere&ccedil;o de e-mail v&aacute;lido.';
+$lang['valid_emails'] = 'O campo %s deve conter todos os endere&ccedil;os de e-mails v&aacute;lidos.';
+$lang['valid_url'] = 'O campo %s deve conter uma URL v&aacute;lida.';
+$lang['valid_ip'] = 'O campo %s deve conter um IP v&aacute;lido.';
 $lang['min_length'] = 'O campo %s deve ter pelo menos %s caracteres em comprimento.';
-$lang['max_length'] = 'O campo %s não pode exceder %s caracteres de tamanho.';
+$lang['max_length'] = 'O campo %s n&atilde;o pode exceder %s caracteres de tamanho.';
 $lang['exact_length'] = 'O campo %s deve ter exatamente %s caracteres em comprimento.';
 $lang['alpha'] = 'O campo %s deve conter somente caraceteres do alfabeto.';
-$lang['alpha_numeric'] = 'O campo %s deve conter somente caracteres alfa-numéricos.';
-$lang['alpha_dash'] = 'O campo %s deve conter somente caracteres alfa-numéricos, sublinhados, e traços.';
-$lang['numeric'] = 'O campo %s deve conter somente números.';
-$lang['is_numeric'] = 'O campo %s deve conter somente caracteres numéricos.';
-$lang['integer'] = 'O campo %s deve conter um número inteiro.';
-$lang['matches'] = 'O campo %s não corresponde ao campo %s.';
-$lang['is_natural'] = 'O campo %s deve conter apenas números positivos.';
-$lang['is_natural_no_zero'] = 'O campo %s deve conter um número maior que zero.';
+$lang['alpha_numeric'] = 'O campo %s deve conter somente caracteres alfa-num&eacute;ricos.';
+$lang['alpha_dash'] = 'O campo %s deve conter somente caracteres alfa-num&eacute;ricos, sublinhados, e tra&ccedil;os.';
+$lang['numeric'] = 'O campo %s deve conter somente n&uacute;meros.';
+$lang['is_numeric'] = 'O campo %s deve conter somente caracteres num&eacute;ricos.';
+$lang['integer'] = 'O campo %s deve conter um n&eacute;mero inteiro.';
+$lang['matches'] = 'O campo %s n&atilde;o corresponde ao campo %s.';
+$lang['is_natural'] = 'O campo %s deve conter apenas n&uacute;meros positivos.';
+$lang['is_natural_no_zero'] = 'O campo %s deve conter um n&uacute;mero maior que zero.';
 ?>
